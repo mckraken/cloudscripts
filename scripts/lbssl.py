1ceb1ca42aa4595488520dbe00a01578f32ee76a
McKraken <mckraken@mckraken.net>
2015-05-11 17:27:48 -0500

    Add in copyright text

--- scripts/lbssl.py
+++ scripts/lbssl.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
-
 #
+# lbssl.py - Version 0.5
+# Rackspace Load Balancer SSL Management Script
+# Copyright (C) 2015 Stephen McCracken - mckraken@mckraken.net
 #
+# Git repository available at http://github.com/mckraken/cloudscripts
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
 
